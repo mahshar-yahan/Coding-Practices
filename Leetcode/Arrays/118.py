7beb244e0772ea44095f03fe29393e485184938e
Mahshar Yahan <136628038+mahshar-yahan@users.noreply.github.com>
2024-12-09 17:35:19 +0600

    118

--- Leetcode/Arrays/118.py
+++ Leetcode/Arrays/118.py
@@ -1,11 +1,21 @@
-n = 5
+n = 4
 # out = [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 # print(out[4])
+temp1 = [[1]]
 test = [[1],[1,1]]
-for i in range(5):
-    print("i print =",i)
+for i in range(n):
     if i >=2 :
         col = test[i-1]
+        temp = []
         for j in range(i+1):
-            
-# print(test)
+            if j ==0: 
+                temp.append(1)
+            elif j==len(col):
+                temp.append(1)
+            else:
+                temp.append(col[j-1]+col[j])
+        test.append(temp)
+if n == 1:
+    print(temp1)
+else:    
+    print(test)
